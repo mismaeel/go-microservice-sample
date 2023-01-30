83ac5ce8681f10897a3da5a2f3e62c4140634829
mismaeel <mohamed.ali.ismaeel@gmail.com>
2023-01-30 15:56:23 +0100

    Update models.go

--- movies/models/models.go
+++ movies/models/models.go
@@ -15,6 +15,5 @@ type (
 		Director  string        `json:"director"`
 		Actors    []string      `json:"actors"`
 		CreatedOn time.Time     `json:"createdAt,omitempty"`
-		Review    string       `json:"review,omitempty"`
 	}
 )
